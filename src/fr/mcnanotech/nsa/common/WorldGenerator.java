b2fe5be822d1fa298632a98632edfd74be6bb90e
letherman255 <matboularot@yahoo.fr>
2014-10-25 18:32:24 +0200

    ore gen fix

--- src/fr/mcnanotech/nsa/common/WorldGenerator.java
+++ src/fr/mcnanotech/nsa/common/WorldGenerator.java
@@ -11,49 +11,52 @@ import cpw.mods.fml.common.IWorldGenerator;
 public class WorldGenerator implements IWorldGenerator
 {
 
-	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
-	{
-		switch(world.provider.dimensionId)
-		{
-		case -1:
-			generateNether(world, random, chunkX * 16, chunkZ * 16);
-			break;
-		case 0:
-			generateSurface(world, random, chunkX * 16, chunkZ * 16);
-			break;
-		case 1:
-			generateEnd(world, random, chunkX * 16, chunkZ * 16);
-			break;
-		}
-	}
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
+    {
+        switch(world.provider.dimensionId)
+        {
+            case -1:
+                generateNether(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            case 0:
+                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            case 1:
+                generateEnd(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            default:
+                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+                break;
+        }
+    }
 
-	private void generateEnd(World world, Random random, int i, int j)
-	{
-		// TODO Auto-generated method stub
+    private void generateEnd(World world, Random random, int i, int j)
+    {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	private void generateSurface(World world, Random random, int i, int j)
-	{
+    private void generateSurface(World world, Random random, int i, int j)
+    {
 
-		for(int k = 0; k < 10; k++)
-		{
+        for(int k = 0; k < 10; k++)
+        {
 
-			int firstBlockXCoord = i + random.nextInt(16);
-			int firstBlockYCoord = random.nextInt(64);
-			int firstBlockZCoord = j + random.nextInt(16);
+            int firstBlockXCoord = i + random.nextInt(16);
+            int firstBlockYCoord = random.nextInt(64);
+            int firstBlockZCoord = j + random.nextInt(16);
 
-			(new WorldGenMinable(NanotechServerAddon.overworldQuartz, 13)).generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
+            (new WorldGenMinable(NanotechServerAddon.overworldQuartz, 13)).generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
 
-		}
+        }
 
-	}
+    }
 
-	private void generateNether(World world, Random random, int i, int j)
-	{
-		// TODO Auto-generated method stub
+    private void generateNether(World world, Random random, int i, int j)
+    {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
