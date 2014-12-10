1a4f4253f697ed72c875693124c4902c5eb1fdf7
letherman255 <matboularot@yahoo.fr>
2014-12-10 18:56:09 +0100

    drop amount

--- src/fr/mcnanotech/nsa/common/event/EventHandler.java
+++ src/fr/mcnanotech/nsa/common/event/EventHandler.java
@@ -1,10 +1,16 @@
 package fr.mcnanotech.nsa.common.event;
 
+import java.util.Random;
+
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.player.PlayerDropsEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHandler
@@ -14,7 +20,9 @@ public class EventHandler
     {
         if(event.entity instanceof EntityVillager)
         {
-            event.drops.add(new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, new ItemStack(Items.emerald)));
+            Random rand = new Random();
+            int randomNum = rand.nextInt((5 - 1) + 1) + 1;
+            event.drops.add(new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, new ItemStack(Items.emerald, randomNum)));
         }
     }
 }
